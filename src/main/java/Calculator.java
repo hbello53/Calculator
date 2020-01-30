c6db8f19c89b05f00a7eb06d2d767ce1ef741782
HABIB BELLO - Student <G00324434@gmit.ie>
2020-01-30 11:57:47 +0000

    First commit in my Calculator repository

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+
+    public static void main ( String[] args ) {
+        int firstNum = 5;
+        int secondNum = 3;
+        int sum;
+
+        sum = firstNum + secondNum;
+
+        System.out.println(sum );
+    }
+}
